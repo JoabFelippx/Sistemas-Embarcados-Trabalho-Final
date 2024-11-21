5287e32eaec43c3ed1d567d72d6b39cc34721d9e
MiguelGrigorio <miguelgrigorio@hotmail.com>
2024-11-21 08:14:43 -0300

    DHT specifications

--- DHT11_reader.ino
+++ DHT11_reader.ino
@@ -6,13 +6,13 @@ DHTesp dhtSensor;
 
 void setup() {
   Serial.begin(115200);
-  dhtSensor.setup(DHT_PIN, DHTesp::DHT11);
+  dhtSensor.setup(DHT_PIN, DHTesp::DHT11); // In Wokwi DHT22
 }
 
 void loop() {
   TempAndHumidity  data = dhtSensor.getTempAndHumidity();
-  Serial.println("Temp: " + String(data.temperature, 2) + "°C");
-  Serial.println("Humidity: " + String(data.humidity, 1) + "%");
+  Serial.println("Temperatura: " + String(data.temperature, 2) + "°C");
+  Serial.println("Umidade: " + String(data.humidity, 1) + "%");
   Serial.println("---");
-  delay(2000); // Wait for a new reading from the sensor (DHT22 has ~0.5Hz sample rate)
+  delay(1000);  // DHT11 1000ms (or 1Hz), DHT22 2000ms (or 0.5Hz).
 }
